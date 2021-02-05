94537435d3e21c207cb5edbcf8bf6fbb29a3751f
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-05 00:42:11 -0600

    feat: generated functions to clean logic and encapsulated process

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function nuevoRegistro() {
+    // Se solicita la usuario los datos necesarios para generar la instancia
+    let nombre = prompt('Nombre:')
+    let edad = prompt('Edad:')
+    let grado = prompt('Grado:')
+    let grupo = prompt('Grupo:')
+
+    // Retornamos la instancia de la clase alumno
+    return new Alumno(nombre, edad, grado, grupo);
+}
+
+function preguntaUsuario() {
+    // Retornamos la respuesta del promp que nos proporciona el usuarios
+    return prompt('Desea registrar otro alumno [s] [n]');
+}
+
+function reporte(lista) {
+    // Mensaje para informar al usuario que se imprimirá la lista de registros
+    console.info('Lista de Alumnos registrados')
+
+    // Se invoca el metodo muestraContenido, ya que este permite imprimir todos los registros existentes
+    lista.muestraContenido();
+}
