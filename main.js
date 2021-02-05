e847861be3b6f0ddceb995612ec0646e0f0f729b
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-05 00:48:08 -0600

    feat: deleted do block

--- main.js
+++ main.js
@@ -3,13 +3,10 @@
 // Se generá la instancia de Lista para poder tener acceso a la lista de registro
 const registrador = new Lista();
 
-// Se ocupa un do while, ya que pretendemos que el usuario nos entregue al menos un registro
-do {
-    // Se ocupa el metodo agregaRegistro para guardar la instancia generada en la lista de registros
-    registrador.agregaRegistro(nuevoRegistro());
-} 
 // Si la respuesta es 's' (y solo la s minuscula, una 'S' terminaria el ciclo)
-while(preguntaUsuario() === 's')
+while(preguntaUsuario() === 's') {
+    registrador.agregaRegistro(nuevoRegistro());
+}
 
 // imprimo todos los registros de la lista
 reporte(registrador);
