7df04a775b9d226f497a4c62cfcd9fe24030f561
tw1ttt3r <tw1ttt3r@gmail.com>
2021-02-05 00:31:41 -0600

    feat: generated persona file to hold Persona class

---
+++ persona.js
@@ -0,0 +1,9 @@
+class Persona {
+
+    constructor(nombre, edad) {
+        // Se generaron solo dos propiedades que cualquier persona podría tener
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
